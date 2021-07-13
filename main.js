7c7e1ae47edd418d3e819158e14b4e6eb5794bec
Vihaan875 <76944903+Vihaan875@users.noreply.github.com>
2021-07-13 16:53:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:100
+})
+
+camera=document.getElementById("camera");
+
+webcam.attach('#camera');
+
+function takeSnapshot()
+{
+    webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version',ml5.version);
+
+    classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JtCI9x0qz/',modelLoaded);
